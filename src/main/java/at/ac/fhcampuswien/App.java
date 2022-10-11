ae81b76cbb00010ffe8b5df698ff460d44fc7912
000Mousavi <zahra.mousavi@stud.fh-campuswien.ac.at>
2022-10-12 00:58:43 +0200

    Task 3  und Task 4

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -11,35 +11,48 @@ public class App {
         double number;
         int count = 1;
         double max = 0;
-       do {
-           System.out.print("Number " + count + ": ");
-           number = scanner.nextFloat();
-           //System.out.printf
+        do {
+            System.out.print("Number " + count + ": ");
+            number = scanner.nextFloat();
+            //System.out.printf
 
-           if (count == 1 && number <= 0) {
-               System.out.println("No number enterd.");
-               break;
-           }
-           if (number > max) {
-               max = number;
-           }
-           if (number <= 0) {
-               System.out.printf("The largest number is "+ "%.2f", max);
-               System.out.println();
-           }
-           count++;
-       } while (number > 0);
+            if (count == 1 && number <= 0) {
+                System.out.println("No number enterd.");
+                break;
+            }
+            if (number > max) {
+                max = number;
+            }
+            if (number <= 0) {
+                System.out.printf("The largest number is " + "%.2f", max);
+                System.out.println();
+            }
+            count++;
+        } while (number > 0);
 
     }
 
     //todo Task 2
-    public void stairs(){
-        // input your solution here
+    public void stairs() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("n: ");
+        int k = 1;
+        int rows = scanner.nextInt();
+        if (rows <= 0) {
+            System.out.println("invalid number!");
+        } else {
+            for (int i = 1; i <= rows; i++) {
+                for (int j = 0; j < 1; j++) {
+                    System.out.print(k++ + " ");
+                }
+            }
+            System.out.println();
+        }
     }
 
     //todo Task 3
-    public void printPyramid(){
-        for ( int i = 0; i < 6 ; i++) {
+    public void printPyramid() {
+        for (int i = 0; i < 6; i++) {
             for (int k = 0; k < 6 - i; k++) {
                 System.out.print(" ");
             }
@@ -51,8 +64,39 @@ public class App {
     }
 
     //todo Task 4
-    public void printRhombus(){
-        // input your solution here
+    public void printRhombus() {
+        Scanner scann = new Scanner(System.in);
+        System.out.print("h: ");
+        int h = scann.nextInt();
+        System.out.print("c: ");
+        char c = scann.next().charAt(0);
+        if (h%2 == 0) {
+            System.out.println("Invalid number!");
+        } else {
+            for (int i = 1; i <= h/2 + 1; i++){
+                for(int j = h/2; j >= i; j--){
+                    System.out.print(" ");
+                }
+                for(int k = i; k > 0; k--){
+                    System.out.print((char) (c-k+1));
+                }
+                for (int l = 1; l<i; l++){
+                    System.out.print((char) (c - l));
+                }
+                System.out.println();
+            }
+            for (int i = h/2; i > 0; i--) {
+                for (int j = h/2; j >= i; j--){
+                    System.out.print(" ");
+                }
+                for (int k = i; k > 0; k--){
+                    System.out.print((char) (c-k+1));
+                }
+                for (int l = 1; l <i; l++)
+                    System.out.print((char) (c - l));
+            }
+            System.out.println();
+        }
     }
 
     //todo Task 5
